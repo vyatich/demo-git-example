16e4d55d833d91fdec51d5cdd349073109fe975c
vyatich <vik_ivy@mail.ru>
2025-01-21 20:59:18 +0300

    addd space

--- src/main/java/ru/jvmbrain/ImmutableCollection.java
+++ src/main/java/ru/jvmbrain/ImmutableCollection.java
@@ -11,6 +11,7 @@ public class ImmutableCollection {
 
         System.out.println(isContains(names, null));
         System.out.println(isContains(immutableNames, null));
+
     }
 
     private static boolean isContains(List<String> list, String element) {
