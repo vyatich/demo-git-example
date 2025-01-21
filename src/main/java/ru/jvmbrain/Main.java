dbbd3a1189eaee97f595eb2c6cf406a13ce61d1f
vyatich <vik_ivy@mail.ru>
2025-01-21 20:56:17 +0300

    JVMBRAIN-35 add get random string

--- src/main/java/ru/jvmbrain/Main.java
+++ src/main/java/ru/jvmbrain/Main.java
@@ -1,10 +1,13 @@
 package ru.jvmbrain;
 
+import java.nio.charset.Charset;
 import java.util.Optional;
+import java.util.Random;
 
 @SuppressWarnings("all")
 public class Main {
 
+    private static final Random random = new Random();
     private static Integer defaultCount;
 
     public static void main(String[] args) {
@@ -16,5 +19,11 @@ public class Main {
         return Optional.ofNullable(count)
                 .map(current -> current * 2)
                 .orElse(defaultCount * 2);
+    }
+
+    public static String getRandom(int length) {
+        byte[] array = new byte[length];
+        random.nextBytes(array);
+        return new String(array, Charset.forName("UTF-8"));
     }
 }
