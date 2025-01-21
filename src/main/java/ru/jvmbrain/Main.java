03e4e9a47c41f6a62b0c3b13341573c3712bdd3d
vyatich <vik_ivy@mail.ru>
2025-01-21 20:47:56 +0300

    JVMBRAIN-25 add static methot get random

--- src/main/java/ru/jvmbrain/Main.java
+++ src/main/java/ru/jvmbrain/Main.java
@@ -1,11 +1,13 @@
 package ru.jvmbrain;
 
 import java.util.Optional;
+import java.util.Random;
 
 @SuppressWarnings("all")
 public class Main {
 
     private static Integer defaultCount;
+    private static final Random random = new Random();
 
     public static void main(String[] args) {
         System.out.println(returnMultiplyNumber(2));
@@ -16,5 +18,9 @@ public class Main {
         return Optional.ofNullable(count)
                 .map(current -> current * 2)
                 .orElse(defaultCount * 2);
+    }
+
+    public static Integer getRandom() {
+        return random.nextInt();
     }
 }
